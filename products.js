893078e2540f2133bea995295d4bc35418eedb84
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-03-14 22:41:32 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 900
+  },
+  {
+    id: 2,
+    name: "Headphones",
+    price: 200
+  },
+  {
+    id: 3,
+    name: "Smartphone",
+    price: 600
+  },
+  {
+    id: 4,
+    name: "Keyboard",
+    price: 120
+  }
+];
